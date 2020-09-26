ff279a266a576df2f39612bb073338b7b2a7f7c0
DieEin <marinov.tom@gmail.com>
2020-09-27 02:09:45 +0300

    dynamic programming - fibonacci memorization method

---
+++ DynamicProgramming/FibonacciMemorization.java
@@ -0,0 +1,47 @@
+import java.util.HashMap;
+
+public class FibonacciMemorization
+{
+	private HashMap<Integer, Integer> dataStorage = new HashMap<Integer, Integer>();
+
+	private void initialize()
+	{
+		dataStorage.put(0, 0);
+		dataStorage.put(1, 1);
+	}
+
+	public FibonacciMemorization()
+	{
+		initialize();
+	}
+
+	private int fibonacci(int index)
+	{
+		if(!dataStorage.containsKey(index))
+		{
+			int newValue;
+
+			if(index <= 1)
+			{
+				newValue = index;
+				dataStorage.put(index, newValue);
+
+				return newValue;
+			}
+
+			newValue = fibonacci(index - 1) + fibonacci(index - 2);
+			dataStorage.put(index, newValue);
+
+			return newValue;
+		}
+
+		return dataStorage.get(index);
+	}
+
+	public static void main(String[] args)
+	{
+		FibonacciMemorization fibonacciMemorization = new FibonacciMemorization();
+
+		System.out.println(fibonacciMemorization.fibonacci(6));
+	}
+}
